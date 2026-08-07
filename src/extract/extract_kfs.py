83822391ca99aef87101621a4cdf6c8ea8472e89
Janelle Akinyi <janellekylieakinyi@gmail.com>
2026-08-07 20:07:47 +0300

    Explanation of extract_kfs.py

---
+++ src/extract/extract_kfs.py
@@ -0,0 +1,16 @@
+"""
+We're not writing extract_kfs.py.
+
+Why?
+
+Unlike GBIF and IUCN,
+
+GBIF → API
+IUCN → API
+
+KFS is already downloaded.
+
+So extraction is already done.
+
+We begin directly with the transform stage.
+"""
